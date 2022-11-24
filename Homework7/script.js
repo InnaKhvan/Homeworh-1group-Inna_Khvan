720436a55c00cc66c7e367034e81a5c97c7f5f6c
Inna Khvan <in.khvan@bk.ru>
2022-11-24 08:22:24 +0600

    add new data in script.js

--- Homework7/script.js
+++ Homework7/script.js
@@ -1,39 +1,52 @@
-
 let pizza = {
-    ingredients1: 'dough',
-    ingredients2: 'salami',
-    priсe: 150,
+    ingredients: ['dough', 'salami', 'salt', 'vegetables'],
+    price: 150,
+    salt: 2,
+    type: 'italian',
+    discount: 15,
 };
+
 let buuza = {
-    ingredients1: 'dough',
-    ingredients2: 'mutton',
-    priсe: 50,
+    ingredients: ['dough', 'meat', 'mutton'],
+    price: 90,
+    salt: 2,
+    type: 'buryat',
+    discount: 25,
 };
+
 let pasta = {
-    ingredients1: 'souce',
-    ingredients2: 'spaghetti',
+    ingredients: ['souce', 'spaghetti'],
     priсe: 100,
+    salt: 3,
+    type: 'italian',
+    discount: 35,
 };
 let cake = {
-    ingredients1: 'dough',
-    ingredients2: 'sugar',
+    ingredients: ['dough', 'sugar', 'fruit'],
     priсe: 120,
+    salt: 1,
+    type: 'nationwide',
+    discount: 25,
 };
 
+let cakePrice = `Cake's price with discount = ${cake.priсe - (cake.priсe * (cake.discount / 100))}`;
+alert(cakePrice);
 
 let food = [pizza, buuza, pasta, cake];
-food.splice(1, 1); //удалили buuza
+
+food.splice(1, 1); //delete buuza
+
 alert(JSON.stringify(food));
 
-delete buuza.ingredients1;
-food.unshift(buuza);
+buuza.ingredients.splice(2, 1); //delete mutton
 
-alert(buuza.ingredients1);
+alert(buuza.ingredients);
+food.unshift(buuza); //add buuza
+
 alert(JSON.stringify(food));
 
-
-let myPriсe = `All price = ${pizza.priсe + buuza.priсe + pasta.priсe + cake.priсe}`;
+let myPriсe = `All price = ${pizza.price + buuza.price + pasta.priсe + cake.priсe}`;
 alert(myPriсe);
 
-food.splice(1, 3);
-alert(JSON.stringify(food));
+food.splice(1, 3); //delete all except buuza
+alert(JSON.stringify(food)) 
