96bd06f263184eb141480559a022450ae1eb05e9
Inna Khvan <in.khvan@bk.ru>
2022-12-05 16:26:24 +0600

    change file script.js add profit in every food

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 for (let i = 2; i <= 10; i++) {
     if (i % 2 == 0) {
-        alert(i);
+        console.log(i);
     }
 }
 
 for (i = 0; ; i++) {
     if (i === 5)
         break; {
-        alert(i);
+        console.log(i);
     }
 }
 
@@ -26,53 +26,41 @@ let pizza = {
     name: 'Пицца',
     ingredients: ['dough', 'salami', 'vegetables'],
     price: 150,
+    costPrice: 0,
+    profit: 0,
 };
 
 let buuza = {
     name: 'Буузы',
     ingredients: ['dough', 'meat', 'mutton'],
     price: 90,
+    costPrice: 0,
+    profit: 0,
 };
 
 let pasta = {
     name: 'Паста',
     ingredients: ['souce', 'spaghetti'],
-    priсe: 100,
+    price: 100,
+    costPrice: 0,
+    profit: 0,
 };
 
 let cake = {
     name: 'Торт',
     ingredients: ['dough', 'sugar', 'fruit'],
-    priсe: 120,
+    price: 120,
+    costPrice: 0,
+    profit: 0,
 };
 
-let pizzaSum = 0;
-for (let i = 0; i < pizza.ingredients.length; i++) {
-    let name = pizza.ingredients[i];
-    pizzaSum += ingPrice[name];
+let food = [pizza, buuza, pasta, cake];
+
+for (let i = 0; i < food.length; i++) {
+    for (let number = 0; number < food[i].ingredients.length; number++) {
+        let name = food[i].ingredients[number];
+        food[i].costPrice += ingPrice[name];
+    }
+    food[i].profit = food[i].price - food[i].costPrice;
+    alert('Профит для блюда "' + food[i].name + '" - ' + food[i].profit);
 }
-alert(`Себестоимость пиццы = ${pizzaSum}`);
-
-let buuzaSum = 0;
-for (let i = 0; i < buuza.ingredients.length; i++) {
-    let name = buuza.ingredients[i];
-    buuzaSum += ingPrice[name];;
-}
-alert(`Себестоимость бууз = ${buuzaSum}`);
-
-let pastaSum = 0;
-for (let i = 0; i < pasta.ingredients.length; i++) {
-    let name = pasta.ingredients[i];
-    pastaSum += ingPrice[name];;
-}
-alert(`Себестоимость пасты = ${pastaSum}`);
-
-let cakeSum = 0;
-for (let i = 0; i < cake.ingredients.length; i++) {
-    let name = cake.ingredients[i];
-    cakeSum += ingPrice[name];;
-}
-alert(`Себестоимость торта = ${cakeSum}`);
-
-
-alert(`Профит для каждого блюда: пицца - ${pizza.price - pastaSum}, буузы - ${buuza.price - buuzaSum}, паста - ${pasta.priсe - pastaSum}, торт - ${cake.priсe - cakeSum}.`)
