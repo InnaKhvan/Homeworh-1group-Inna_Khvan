c14cdd291868322167ad26a3e72304eb1ec90e9b
Inna Khvan <in.khvan@bk.ru>
2022-12-16 22:05:50 +0600

    changes

--- Homework10.js
+++ Homework10.js
@@ -4,10 +4,8 @@ function info(a, b) {
     }
     else { return b };
 }
-let a = prompt('Введи 1ое число');
-let b = prompt('Введи 2ое число');
-a = Number(a);
-b = Number(b);
+let a = +prompt('Введи 1ое число');
+let b = +prompt('Введи 2ое число');
 let result = info(a, b);
 alert('Наименьшее число - ' + result);
 
@@ -24,7 +22,7 @@ function calcFoodProfit(ingredients, ingredientsCost, foodPrice) {
 }
 
 let text = "";
-for (let i = 0; i < foodObj.food.length; i++) {
-    let profit = '"'+ foodObj.food[i].name + '" - ' + (calcFoodProfit(foodObj.food[i].ingredients, ingPrice, foodObj.food[i].price)) + '. ';
+for (let i = 0; i < foodObj.length; i++) {
+    let profit = '"'+ foodObj[i].name + '" - ' + (calcFoodProfit(foodObj[i].ingredients, ingPrice, foodObj[i].price)) + '. ';
 text += profit;}
 alert('Профит для блюда ' + text)
